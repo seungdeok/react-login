dc1f2d5910d7eed070e7a5c9cae978a39b2882d4
seungdeok <akk9132@naver.com>
2023-10-23 22:11:57 +0900

    feat: Configure server setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: /^(http:\/\/localhost:[0-9]{4})|(http:\/\/127.0.0.1:[0-9]{4})$/,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
+  });
+
+  app.use(
+    session({
+      secret: 'react-login-server',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 24 * 6 * 60 * 10000,
+        sameSite: 'lax',
+        httpOnly: true,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
